97a210d5d3d5fb66c9b477e6ceb7e38565e7fe34
James Haff <jehaff@James-Haffs-MBP.attlocal.net>
2015-09-12 19:00:44 -0500

    save finally working

--- paintingRest/public/javascripts/core.js
+++ paintingRest/public/javascripts/core.js
@@ -13,27 +13,31 @@ paintApp.controller('paintCtrl', function($scope, $http){
   };
 
   $scope.create = function(username, email, password, canvas) {
-    $http.post($scope.url, {username: username, email: email, password: password, canvas: canvas}).success(function(data, status){
+
+    getCanvas = function(){
+    var canvas = document.getElementById("myCanvas");
+    console.log(canvas);
+    context = canvas.getContext('2d');
+    dataURL = canvas.toDataURL("image/png");
+    console.log(dataURL);
+    var image = new Image();
+    image.src = canvas.toDataURL("image/png");
+    return image.src;
+   }
+
+    $scope.canvas = getCanvas();
+
+    $http.post($scope.url, {username: username, email: email, password: password, canvas: $scope.canvas}).success(function(data, status){
+
+        console.log(data);
+
+        return status;
 
 
-      var canvas = document.getElementById("myCanvas");
-      console.log(canvas);
-      context = canvas.getContext('2d');
-      dataURL = canvas.toDataURL("image/png");
-      console.log(dataURL);
-      var image = new Image();
-      image.src = canvas.toDataURL("image/png");
-
-
-
-      $scope.fetch();
     });
-    
+
+
   };
-
-
-
-
 
 
 $scope.fetch();
