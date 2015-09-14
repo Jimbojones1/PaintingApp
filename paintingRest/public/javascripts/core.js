3d82aa10580ece086149ce1989a3b5f36f1acac8
James Haff <jehaff@James-Haffs-MacBook-Pro.local>
2015-09-14 16:10:57 -0500

    load working

--- paintingRest/public/javascripts/core.js
+++ paintingRest/public/javascripts/core.js
@@ -5,6 +5,9 @@ var paintApp = angular.module('paintApp', []);
 paintApp.controller('paintCtrl', function($scope, $http){
 
   $scope.url = '/api/users';
+  $scope.data = {};
+  $scope.img;
+
 
   $scope.fetch = function(){
     $http.get($scope.url).success(function(data){
@@ -32,14 +35,25 @@ paintApp.controller('paintCtrl', function($scope, $http){
         console.log(data);
 
         return status;
-
-
     });
 
+  };
 
+   $scope.drawCanvas = function (f) {
+    console.log('canvas is working');
+    var canvas = document.getElementById("myCanvas");
+    context = canvas.getContext('2d');
+    context.clearRect(0, 0, app.canvas.width(), app.canvas.height());
+    var image = new Image();
+    console.log($scope.data.activeImg)
+    image.src = $scope.data.activeImg;
+    console.log(image);
+    context.drawImage(image, 0, 0);
   };
 
 
 $scope.fetch();
+$('#dd').on('change', function() { console.log($(this).prop('selected')) } );
+
 
 })
